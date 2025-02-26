94fa63dbab282458ec9519ff93e619fb2ee40a35
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2025-02-26 22:28:58 +0500

    price updated

--- app/api/stripe-checkout/route.ts
+++ app/api/stripe-checkout/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
             product_data: {
               name: name,
             },
-            unit_amount: price,
+            unit_amount: price*100,
           },
           quantity: 1,
         },
