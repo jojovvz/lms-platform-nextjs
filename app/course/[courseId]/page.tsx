a2e2f69864bfe35f8670cf12231dab758062b3d7
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2025-02-26 22:34:28 +0500

    price updated

--- app/course/[courseId]/page.tsx
+++ app/course/[courseId]/page.tsx
@@ -1,8 +1,23 @@
-import Success from '@/components/ui/Success'
+'use client'
+
+import { getTransactionDetails } from '@/app/actions/transaction';
 import React from 'react'
 
 const page = () => {
-  return <Success />
+  const [details, setDetails] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await getTransactionDetails();
+      const data = await response.json();
+      setDetails(data);
+    }
+    fetchData();
+  }, []);
+  console.log("Details: ", details);
+  return (
+    <div>page</div>
+  )
 }
 
 export default page
